3d3cee9872ab1ba73f195083a630406d875319bd
maybeags <suren0709@gmail.com>
2025-02-18 16:08:05 +0900

    feat: 20250218 lesson process

--- src/main/java/chapter13_abstraction/abstract_class/TabletFactory.java
+++ src/main/java/chapter13_abstraction/abstract_class/TabletFactory.java
@@ -1,4 +1,19 @@
 package chapter13_abstraction.abstract_class;
+// 뻘건거 다 없애주세요.
+public class TabletFactory extends Factory{
+    public TabletFactory(String name) {
+        super(name);
+    }
 
-public class TabletFactory {
+    @Override
+    public void produce(String model) {
+        System.out.println("[ " + model + " ] 모델 태블릿을 생산합니다.");
+
+    }
+
+    @Override
+    public void manage() {
+        System.out.println("태블릿 공장을 관리합니다.");
+
+    }
 }
